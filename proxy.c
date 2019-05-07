4d3867855ec501f5c4f173240e708e6a15727078
justin-shearson <justin.shearson@case.edu>
2019-05-07 11:41:29 -0400

    Added commenting

--- proxy.c
+++ proxy.c
@@ -27,7 +27,9 @@ int main(int argc, char *argv[])
 
 	fd_set livesdset, servsdset, workingsdset;   /* set of live client sockets and set of live http server sockets */
 
-	int maxsd = 10;  //TODO: define largest file descriptor number used for select 
+	// Largest file descriptor for the servsdset and livesdset
+	int maxsd = 10;
+	//Largest file descriptor for the workingfdset
 	int maxcombinedsd = (2 * maxsd);
 	struct pair * table = malloc(sizeof(struct pair)); /* table to keep client<->server pairs */
 
@@ -47,11 +49,11 @@ int main(int argc, char *argv[])
 
 	table->next = NULL;
 
-  	/* TODO: initialize all the fd_sets and largest fd numbers */
+  	// initialize all the fd_sets and largest fd numbers
 	FD_ZERO(&livesdset);
 	FD_ZERO(&servsdset);
 	FD_ZERO(&workingsdset);
-	FD_SET(servsock, &servsdset);
+	FD_SET(servsock, &servsdset); //add server socket to the server fd_set
 	FD_SET(maxsd, &livesdset);
 	FD_SET(maxsd, &servsdset);
 	FD_SET(maxcombinedsd, &workingsdset);
@@ -59,35 +61,50 @@ int main(int argc, char *argv[])
 
 	while (1) {
 		int frsock = 0;
-    	/* TODO: combine livesdset and servsdset 
-     	* use the combined fd_set for select */
+    	/*
+    	  Combines the two fd_sets into one working fd_set. This working fd_set is then used in the select function
+    	    to check if there are any active sockets.
+    	*/
 		for(int i = 0; i < maxsd; i++){
 			if(FD_ISSET(i, &livesdset) || (FD_ISSET(i, &servsdset))){
 				printf("Set Socks: %d\n", i);
 				FD_SET(i, &workingsdset);
 			}
 		}
-		error = select(maxcombinedsd, &workingsdset, NULL, NULL, NULL);
-		if(error == -1) {
+
+		/*
+		  Selects from the workingsdset and monitors the set until a request is made to the server.
+		*/
+		if(select(maxcombinedsd, &workingsdset, NULL, NULL, NULL)== -1) {
 			printf("Error selecting file descriptor: %s\n", strerror(errno));
 			continue;
 		}
-
+		/*
+		  Checks to find what file descriptors in the livesdset are active.
+			If found file descriptor points to the socket for the server, that iteration is skipped.
+		*/
 		for (frsock = 0; frsock < maxsd; frsock++) {
 			if (frsock == servsock) continue;
+			
+			/*
+			  If the socket is set, the server has found a client wanting to make a request through the server
+			    The server then parses the message being requested by the client and then forwards that
+			    request to the website passed by the client.
+			*/
 			if(FD_ISSET(frsock, &livesdset)) {
 	    		/* forward the request */
 				int newsd = sendrequest(frsock);
 				printf("Returned newsd: %d\n", newsd);
 				if (!newsd) {
 					printf("admin: disconnect from client\n");
-					/*TODO: clear frsock from fd_set(s) */
+					// Inserts that socket into the server and live sd sets
 					FD_CLR(frsock, &servsdset);
 					FD_CLR(frsock, &livesdset);
 				} else {
 					/* TODO: insert newsd into fd_set(s) */
 					insertpair(table, newsd, frsock);
 					FD_SET(newsd, &workingsdset);
+					FD_SET(newsd, &servsdset);
 				}
 			} 
 			if(FD_ISSET(frsock, &servsdset)) {
@@ -121,7 +138,7 @@ int main(int argc, char *argv[])
 		}
 
     	/* input from new client*/
-		if(FD_ISSET(servsock, &workingsdset)) {
+		if(FD_ISSET(frsock, &livesdset)) {
 			struct sockaddr_in caddr;
 			socklen_t clen = sizeof(caddr);
 			int csd = accept(servsock, (struct sockaddr*)&caddr, &clen);
